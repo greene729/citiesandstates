80d66f0c6cc778873ab08cc3fa6204ef89545c8e
Joe Rudy <joe.rudy@sbcglobal.net>
2018-10-31 14:15:48 -0400

    initial commit

---
+++ src/main/java/org/wecancodeit/citiesandstates/CitiesandstatesApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.citiesandstates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesandstatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesandstatesApplication.class, args);
+	}
+}
